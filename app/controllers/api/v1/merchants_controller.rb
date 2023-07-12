cf785490b06f6de8a9dc51d54b948a4ce4e9efc2
Jesse Sorman <jessesorman@gmail.com>
2023-07-12 17:40:28 -0400

    refactor search for merchant by name

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,7 +8,18 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def find
-    name = Merchant.where(name == params[:name])
-    render json: MerchantSerializer.new(name)
+    name = Merchant.search_by_name(merchant_params[:name]).first
+    if name.present?
+      render json: MerchantSerializer.new(name)
+    else
+      render json: MerchantSerializer.new(name)
+    end
   end
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name)
+  end
+
 end
---
+++ spec/requests/unrestful_merchants/merchants_request_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "items api" do
+  describe "can find a item by keyword" do
+    it "happy path" do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      item_1 = Item.create(name: "Computer", description: "Computer fo special tasks", unit_price: 1000, merchant_id: merchant_1.id)
+      item_2 = Item.create(name: "Phone", description: "Phone", unit_price: 950, merchant_id: merchant_2.id)
+
+      get "/api/v1/merchants/find?name=#{merchant_1.name}"
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      expect(merchant[:data][:attributes][:name]).to eq(merchant_1.name)
+    end
+    it "sad path" do
+      get "/api/v1/merchants/find?name=dogs"
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      expect(merchant[:data]).to eq(nil)
+    end
+  end
+end
